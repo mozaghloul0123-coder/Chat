7fcdb8141ceb9a359e67f90398b12ca89d53975f
mozaghloul0123-coder <mozaghloul0123@gmail.com>
2026-04-08 22:48:34 +0200

    add google verification and enable static files

--- Div.Link.Project01/Program.cs
+++ Div.Link.Project01/Program.cs
@@ -63,7 +63,7 @@ if (app.Environment.IsDevelopment())
                 app.UseSwaggerUI();
             }
 
-            //app.UseStaticFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
 
             app.UseCors("AllowAll");
